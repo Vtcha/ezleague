3353da8d6431b397b707b481807fbefc1aa6662c
Michael Loring <mdloring@gmail.com>
2014-09-01 03:45:54 -0500

    delete old ezLeague version
    prepping for ezLeague v3.0

--- admin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php session_start();
-	unset($_SESSION['ez_admin']);
-	session_destroy($_SESSION['ez_admin']);
-	header("Location: login.php");
-?>
--- admin/upgrade.php
+++
@@ -1,9 +0,0 @@
-<?php session_start();
-include('lib/db.class.php');
-include('lib/ezleague.class.php');
-
-$ez = new ezLeague();
-
-	$ez->upgrade();
-	
-?>
