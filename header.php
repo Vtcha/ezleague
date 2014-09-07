994da2604cfdfe3eecd604c358d3222d0797c8d4
Michael Loring <mdloring@gmail.com>
2014-09-06 20:53:39 -0500

    update versioning

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <?php session_start();
-define( 'EZL_VERSION', '3.2.3' );
+define( 'EZL_VERSION', '3.2.4' );
 date_default_timezone_set('America/Chicago');
 include('lib/class-db.php');
 include('lib/class-ezleague.php');
