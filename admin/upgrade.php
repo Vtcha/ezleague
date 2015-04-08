a3266d66af9b591c8bfdcbe9e6eef4f01b7368eb
Michael Loring <mdloring@gmail.com>
2015-04-07 23:03:28 -0500

    fix conditional error on upgrade

--- admin/upgrade.php
+++ admin/upgrade.php
@@ -3,14 +3,10 @@ date_default_timezone_set('America/Chicago');
 include('lib/class-db.php');
 include('lib/class-ezadmin.php');
 $ez = new ezAdmin();
-
-/**
- * Upgrades include: Recent Twitter Tweets, Site Favorite Icon v2, Prediction System, Installation v3, Game Edit Short Name, Max League Roster, Mandrill SMTP Settings, Site Timezone, Suspended Teams, Match Server Information, Match Timezone Update
- */    
+    
      if( isset($_SESSION['ez_admin'] ) ) {
      	$ez->run_upgrade();
      	unlink('upgrade.php');
      } else {
      	echo 'You are not an admin';
-     }
-?>
+ 	 }
