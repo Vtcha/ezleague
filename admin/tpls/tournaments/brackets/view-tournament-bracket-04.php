c47fd2db2d2909997657095f498c1458ef4bd5e4
Michael Loring <mdloring@gmail.com>
2015-04-12 23:15:32 -0500

    only show the round 1 match generation button once max teams have been registered

--- admin/tpls/tournaments/brackets/view-tournament-bracket-04.php
+++ admin/tpls/tournaments/brackets/view-tournament-bracket-04.php
@@ -1,27 +1,32 @@
 <?php 
-        $round_1_started    = $ez_tournament->check_if_started( $tournament_id );
-        if( $round_1_started == false ) { ?>
-            <div class="pull-right">
-                <button id="generateRound1Matches" style="margin-right:5px;" data-tournament-id="<?php echo $tournament_id; ?>" data-tournament-teams="<?php echo $tournament['teams']; ?>" class="btn btn-primary btn-xs">Generate Round 1 Matchups</a></button>
-                <button id="clearRound1Matches" class="btn btn-warning btn-xs" style="display:none;">Clear Matches</button> 
-            </div>
-    <?php 
-        } else {
-            $round_1_completed = $ez_tournament->check_if_round_completed( $tournament_id, '1' );
+        if( $current_teams_amount == $max_teams ) {
+            $round_1_started    = $ez_tournament->check_if_started( $tournament_id );
+            if( $round_1_started == false ) { ?>
+                <div class="pull-right">
+                    <button id="generateRound1Matches" style="margin-right:5px;" data-tournament-id="<?php echo $tournament_id; ?>" data-tournament-teams="<?php echo $tournament['teams']; ?>" class="btn btn-primary btn-xs">Generate Round 1 Matchups</a></button>
+                    <button id="clearRound1Matches" class="btn btn-warning btn-xs" style="display:none;">Clear Matches</button> 
+                </div>
+        <?php 
+            } else {
+                $round_1_completed = $ez_tournament->check_if_round_completed( $tournament_id, '1' );
 
-            if( $round_1_completed ) {
-                $round_2_exists    = $ez_tournament->check_if_round_exists( $tournament_id, '2' );
-                if( ! $round_2_exists ) {
-                ?>
-                    <div class="pull-right">
-                        <button id="generateRound2Matches" style="margin-right:5px;" data-tournament-id="<?php echo $tournament_id; ?>" class="btn btn-primary btn-xs">Generate Round 2 Matchups</a></button>
-                    </div>
-            <?php
-                } 
+                if( $round_1_completed ) {
+                    $round_2_exists    = $ez_tournament->check_if_round_exists( $tournament_id, '2' );
+                    if( ! $round_2_exists ) {
+                    ?>
+                        <div class="pull-right">
+                            <button id="generateRound2Matches" style="margin-right:5px;" data-tournament-id="<?php echo $tournament_id; ?>" class="btn btn-primary btn-xs">Generate Round 2 Matchups</a></button>
+                        </div>
+                <?php
+                    } 
+                }
+                
             }
-            
-        } 
-    ?>
+        } else { ?>
+            <div class="pull-right">
+                <em><?php echo $max_teams . ' teams needed to generate matches'; ?></em>
+            </div>
+  <?php } ?>
 </div>
 <div class="panel-body">
 <small>* <em>Round 1</em> matchups can be generated up until a <em>Round 1</em> Match has been completed</small>
